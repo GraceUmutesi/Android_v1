54472d3c05feba02624fc7eac89436b732ba71c3
GraceUmutesi <tesigrace98@gmail.com>
2019-10-13 22:48:49 +0200

    other changes

--- app/src/main/java/com/example/gym_flex/nameListActivity.java
+++ app/src/main/java/com/example/gym_flex/nameListActivity.java
@@ -2,14 +2,17 @@ package com.example.gym_flex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
-
-import butterknife.BindView;
+import android.widget.Toast;
 
 public class nameListActivity extends AppCompatActivity {
-    @BindView(R.id.nextButton) Button mFindnextButtonButton;
-    @BindView(R.id.programText) EditText mprogramTextEditText;
+    private Button mFindProgramButton;
+    private EditText mProgramName;
     GridView gridView;
     String[] details = new String[]{
             "Program","Start","End","Beginner","January","April","Medium","May","August","Pro","September","December"
@@ -22,5 +25,20 @@ public class nameListActivity extends AppCompatActivity {
 
         gridView = (GridView) findViewById(R.id.longGridView);
         gridView.setAdapter(new ProgramAdapter(this, details));
+
+        mProgramName =(EditText) findViewById(R.id.programText);
+        mFindProgramButton = (Button) findViewById(R.id.nextButton);
+        mFindProgramButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                String program = mProgramName.getText().toString();
+                Intent intent = new Intent(nameListActivity.this, ListActivity.class);
+                startActivity(intent);
+
+            }
+
+        });
+
     }
 }
