9d84d36d6cc0f9e83f3abc2079de615b93c9fc35
GraceUmutesi <tesigrace98@gmail.com>
2019-10-13 23:28:13 +0200

    add an adapter for the list

---
+++ app/src/main/java/com/example/gym_flex/ListArrayAdapter.java
@@ -0,0 +1,30 @@
+package com.example.gym_flex;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.Nullable;
+
+public class ListArrayAdapter extends ArrayAdapter{
+    private Context mContext;
+    private String[] mSportNames;
+
+    public ListArrayAdapter(Context mContext, int resource, String[] mSportNames) {
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mSportNames = mSportNames;
+
+    }
+
+    @Override
+    public int getCount() {
+        return mSportNames.length;
+    }
+
+
+    @Override
+    public Object getItem(int position) {
+        String sportNames = mSportNames[position];
+        return String.format("%s",sportNames);
+    }
+}
